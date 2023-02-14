c2577f9e0f5d7aeffff2995c860e6db7619fa072
AlecGoriup <113146110+AlecGoriup@users.noreply.github.com>
2023-02-14 18:07:43 -0500

    User wants addition or subtraction

--- Calculator.java
+++ Calculator.java
@@ -9,8 +9,23 @@ public class Calculator {
         System.out.println("Please enter your second whole number.");
         int wholeNum2 = scan.nextInt();
 
-        int result = wholeNum1 + wholeNum2;
+        scan.nextLine(); // skip newLine char
 
-        System.out.println("Added together: " + wholeNum1 + " + " + wholeNum2 + " = " + result + ".");
+        System.out.println("Would you like to perform addition or subtraction with the two numbers");
+        String choice = scan.nextLine();
+        System.out.println(choice);
+
+        if (choice.equals("addition")) {
+            int result = wholeNum1 + wholeNum2;
+
+            System.out.println("Added together: " + wholeNum1 + " + " + wholeNum2 + " = " + result + ".");
+        }
+
+        if (choice.equals("subtraction")) {
+            int result = wholeNum1 - wholeNum2;
+
+            System.out.println("Subtracted together: " + wholeNum1 + " - " + wholeNum2 + " = " + result + ".");
+        }
+
     }
 }
